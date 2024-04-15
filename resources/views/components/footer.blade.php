9d55a1e6cfcb608ff80bfbf3ee7da7b5e177ea8b
Hadi <Hadi60@gmail.com>
2024-04-15 09:57:52 +0330

    add menu

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -1,3 +1,10 @@
+<input id="my-drawer" type="checkbox" class="drawer-toggle">
+<div class="drawer-side">
+    <label for="my-drawer" class="drawer-overlay"></label>
+    <ul class="menu p-4 w-80 min-h-full bg-base-200 text-base-content">
+        <x-menu />
+    </ul>
+</div>
 <footer class="bg-gray-800 text-white text-center py-4">
-  Copyright &copy; {{ date('Y') }} FreeAmir
+    Copyright &copy; {{ date('Y') }} FreeAmir
 </footer>
---
+++ resources/views/components/menu.blade.php
@@ -0,0 +1,76 @@
+<li><a href="/">{{ __('Home') }}</a></li>
+<li>
+    <details>
+        <summary>عملیات</summary>
+        <ul>
+            <li><a href="">دریافت و پرداخت</li>
+            <li><a href="">ثبت فاکترو فروش</a></li>
+            <li><a href="">ثبت فاکتور خرید</a></li>
+            <li><a href="">اضافه کردن طرف حساب</a></li>
+        </ul>
+    </details>
+</li>
+<li>
+    <details>
+        <summary>حسابداری</summary>
+        <ul>
+            <li><a href="{{ route('transactions.index') }}">{{ __('Transactions') }}</a></li>
+            <li><a href="">فروش ها</a></li>
+            <li><a href="">خرید ها</a></li>
+            <li><a href="">چک ها</a></li>
+        </ul>
+    </details>
+</li>
+<li>
+    <details>
+        <summary>گزارشات</summary>
+        <ul>
+            <li>
+                <details>
+                    <summary>حسابداری</summary>
+                    <ul>
+                        <li><a href="">سند</a></li>
+                        <li><a href="">روزنامه</a></li>
+                        <li><a href="">معین</a></li>
+                        <li><a href="">کل</a></li>
+                        <li><a href="">سود و زیان</a></li>
+                    </ul>
+                </details>
+                </a>
+            </li>
+            <li>
+                <details>
+                    <summary>انبار</summary>
+                    <ul>
+                        <li><a href="{{ route('products.index') }}">{{ __('Products') }}</a></li>
+                        <li><a href="{{ route('product-groups.index') }}">{{ __('Product Groups') }}</a>
+                        </li>
+                    </ul>
+                </details>
+            </li>
+            <li>
+                <details>
+                    <summary>طرف حسابها</summary>
+                    <ul>
+                        <li><a href="">بدهکاران</a></li>
+                        <li><a href="">بستانکاران</a></li>
+                    </ul>
+                </details>
+            </li>
+        </ul>
+    </details>
+</li>
+<li>
+    <details>
+        <summary>مدیریت</summary>
+        <ul>
+            <li><a href="{{ route('subjects.index') }}">{{ __('Subjects') }}</a></li>
+            <li><a href="{{ route('bank-accounts.index') }}">{{ __('Bank Accounts') }}</a></li>
+            <li><a href="{{ route('customers.index') }}">{{ __('Customers') }} </a></li>
+            <li><a href="{{ route('customer-groups.index') }}">{{ __('Customers Groups') }} </a></li>
+            <li><a href="{{ route('banks.index') }}">{{ __('Banks') }}</a></li>
+            <li><a href="">تنظیمات</a></li>
+            <li><a href="">پشتیبانی</a></li>
+        </ul>
+    </details>
+</li>
