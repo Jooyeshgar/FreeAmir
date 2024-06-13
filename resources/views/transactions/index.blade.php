c7fb32560703c803eaaebc95d752b8e4e9990fda
Hadi <Hadi60@gmail.com>
2024-06-13 15:31:58 +0330

    Minor UI change

--- resources/views/transactions/index.blade.php
+++ resources/views/transactions/index.blade.php
@@ -4,40 +4,40 @@
             {{ __('Transactions') }}
         </h2>
     </x-slot>
-    <x-show-message-bags/>
-        <div class="card bg-base-100 shadow-xl">
-            <div class="card-body">
-                <div class="card-actions">
-                    <a href="{{ route('transactions.create') }}" class="btn btn-primary">Create transaction</a>
-                    <table class="table w-full mt-4 overflow-auto">
-                        <thead>
-                        <tr>
-                            @foreach($cols as $col)
-                                <th class="px-4 py-2">{{$col}}</th>
-                            @endforeach
-                            <th class="px-4 py-2">Actions</th>
-                        </tr>
-                        </thead>
-                        <tbody>
-                        @foreach ($documents as $document)
-                            <tr>
-                                <td class="px-4 py-2">{{ $document->id }}</td>
-                                <td class="px-4 py-2">{{ $document->number }}</td>
-                                <td class="px-4 py-2">{{ $document->title }}</td>
-                                <td class="px-4 py-2">{{ $document->created_at }}</td>
-                                <td class="px-4 py-2">
-                                    <a href="{{ route('transactions.edit', $document->id) }}" class="btn btn-sm btn-info">Edit</a>
-                                    <form action="{{ route('transactions.destroy', $document) }}" method="POST" class="inline-block">
-                                        @csrf
-                                        @method('DELETE')
-                                        <button type="submit" class="btn btn-sm btn-error">Delete</button>
-                                    </form>
-                                </td>
-                            </tr>
-                        @endforeach
-                        </tbody>
-                    </table>
-                </div>
+    <x-show-message-bags />
+    <div class="card bg-base-100 shadow-xl">
+        <div class="card-body">
+            <div class="card-actions">
+                <x-button href="{{ route('transactions.create') }}" class="btn-primary">{{ __('Create Document') }}</x-button>
             </div>
+            <table class="table w-full mt-4 overflow-auto">
+                <thead>
+                    <th class="p-2 w-12">{{ __('Number') }}</th>
+                    <th class="p-2">{{ __('Title') }}</th>
+                    <th class="p-2 w-40">{{ __('Sum') }}</th>
+                    <th class="p-2 w-40">{{ __('Date') }}</th>
+                    <th class="p-2 w-40">{{ __('Action') }}</th>
+                </thead>
+                <tbody>
+                    @foreach ($documents as $document)
+                        <tr>
+                            <td class="p-2">{{ $document->number }}</td>
+                            <td class="p-2">{{ $document->title }}</td>
+                            <td class="p-2">{{ $document->transaction->sum('value') }}</td>
+                            <td class="p-2">{{ $document->JalaliDate }}</td>
+                            <td class="p-2">
+                                <a href="{{ route('transactions.edit', $document->id) }}" class="btn btn-sm btn-info">{{ __('Edit') }}</a>
+                                <form action="{{ route('transactions.destroy', $document) }}" method="POST" class="inline-block">
+                                    @csrf
+                                    @method('DELETE')
+                                    <button type="submit" class="btn btn-sm btn-error">{{ __('Delete') }}</button>
+                                </form>
+                            </td>
+                        </tr>
+                    @endforeach
+                </tbody>
+            </table>
         </div>
+    </div>
+    </div>
 </x-app-layout>
