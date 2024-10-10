6426a01b051302b92a05569930669910a9e52d3e
DNAliyari <dnaliyari@jooyeshgar.com>
2024-10-10 14:18:10 +0330

    on hove open menu and responsive

--- resources/views/components/header.blade.php
+++ resources/views/components/header.blade.php
@@ -1,5 +1,5 @@
-<header class="navbar flex justify-between">
-    <div class="flex-none">
+<header class="navbar justify-between">
+    <div class="flex-2 px-2 lg:flex-none">
         <div class="flex items-center bg-gray-200 rounded-xl mx-4 p-1">
             <img src="/images/logo.png" alt="Logo" width="50" class="">
         </div>
@@ -8,31 +8,27 @@
         </ul>
     </div>
 
-    <div class="text-right">
+    <div class="flex flex-1 justify-end px-2">
         <ul class="menu menu-horizontal px-1 bg-gray-200 rounded-xl">
-            <li>
-                <details>
-                    <summary>
-                        {{ session('active-company-id') ? session('active-company-name') . ' - ' . session('active-company-fiscal-year') : __('Please Select a Company') }}
-                    </summary>
-                    <ul>
-                        @foreach (auth()->user()->companies as $company)
-                            <li>
-                                <a href="{{ route('change-company', ['company' => $company->id]) }}">
-                                    {{ $company->name . ' - ' . $company->fiscal_year }}
-                                </a>
-                            </li>
-                        @endforeach
-                    </ul>
-                </details>
+            <li class="dropdown dropdown-hover">
+                <div tabindex="0" role="button">
+                    {{ session('active-company-id') ? session('active-company-name') . ' - ' . session('active-company-fiscal-year') : __('Please Select a Company') }}
+                </div>
+                <ul tabindex="0" class="dropdown-content menu bg-base-100 rounded-box z-[1] w-52 p-2 shadow">
+                    @foreach (auth()->user()->companies as $company)
+                        <li>
+                            <a href="{{ route('change-company', ['company' => $company->id]) }}">
+                                {{ $company->name . ' - ' . $company->fiscal_year }}
+                            </a>
+                        </li>
+                    @endforeach
+                </ul>
             </li>
-            <li>
-                <details>
-                    <summary>{{ Auth::user()->name }}</summary>
-                    <ul>
-                        <li><a href="/logout">{{ __('Logout') }}</a></li>
-                    </ul>
-                </details>
+            <li class="dropdown dropdown-hover">
+                <div tabindex="1" role="button">{{ Auth::user()->name }}</div>
+                <ul tabindex="1" class="dropdown-content menu bg-base-100 rounded-box z-[1] w-52 p-2 shadow">
+                    <li><a href="/logout">{{ __('Logout') }}</a></li>
+                </ul>
             </li>
         </ul>
     </div>
