eaadfe227ad96134fb22896a60e9e680fe4a8eb9
DNAliyari <dnaliyari@jooyeshgar.com>
2024-11-03 14:50:08 +0330

    change in product-groups form

--- resources/views/productGroups/form.blade.php
+++ resources/views/productGroups/form.blade.php
@@ -1,15 +1,11 @@
 <div class="grid grid-cols-2 gap-5">
     <div class="col-span-2 md:col-span-1">
-        <label for="code" class="input input-bordered flex items-center gap-2">
-            {{ __('Account code') }}
-            <input id="code" name="code" type="text" value="{{ old('code', $productGroup->code ?? '') }}" class="grow" placeholder="کد طرف حساب" />
-        </label>
+        <x-input name="code" id="code" title="{{ __('Code') }}" :value="old('code', $productGroup->code ?? '')"
+            placeholder="{{ __('Please insert code') }}" />
     </div>
 
     <div class="col-span-2 md:col-span-1">
-        <label for="name" class="input input-bordered flex items-center gap-2">
-            {{ __('Name') }}
-            <input id="name" name="name" type="text" value="{{ old('name', $productGroup->name ?? '') }}" class="grow" placeholder="نام" />
-        </label>
+        <x-input name="name" id="name" title="{{ __('Name') }}" :value="old('name', $productGroup->name ?? '')"
+            placeholder="{{ __('Please enter name') }}" />
     </div>
 </div>
--- resources/views/productGroups/index.blade.php
+++ resources/views/productGroups/index.blade.php
@@ -4,11 +4,12 @@
             {{ __('Products Group') }}
         </h2>
     </x-slot>
-    <x-show-message-bags/>
+    <x-show-message-bags />
     <div class="card bg-base-100 shadow-xl">
         <div class="card-body">
             <div class="card-actions">
-                <a href="{{ route('product-groups.create') }}" class="btn btn-primary">{{ __("Create product group") }}</a>
+                <a href="{{ route('product-groups.create') }}"
+                    class="btn btn-primary">{{ __('Create product group') }}</a>
             </div>
 
             <table class="table w-full mt-4 overflow-auto">
@@ -21,22 +22,22 @@
                 </thead>
                 <tbody>
 
-                @foreach ($productGroups as $productGroup)
-                    <tr>
-                        <td class="px-4 py-2">{{ $productGroup->code }}</td>
-                        <td class="px-4 py-2">{{ $productGroup->name }}</td>
-                        <td class="px-4 py-2">
-                            <a href="{{ route('product-groups.edit', $productGroup) }}"
-                                class="btn btn-sm btn-info">Edit</a>
-                            <form action="{{ route('product-groups.destroy', $productGroup) }}" method="POST"
+                    @foreach ($productGroups as $productGroup)
+                        <tr>
+                            <td class="px-4 py-2">{{ $productGroup->code }}</td>
+                            <td class="px-4 py-2">{{ $productGroup->name }}</td>
+                            <td class="px-4 py-2">
+                                <a href="{{ route('product-groups.edit', $productGroup) }}"
+                                    class="btn btn-sm btn-info">{{ __('Edit') }}</a>
+                                <form action="{{ route('product-groups.destroy', $productGroup) }}" method="POST"
                                     class="inline-block">
-                                @csrf
-                                @method('DELETE')
-                                <button type="submit" class="btn btn-sm btn-error">Delete</button>
-                            </form>
-                        </td>
-                    </tr>
-                @endforeach
+                                    @csrf
+                                    @method('DELETE')
+                                    <button type="submit" class="btn btn-sm btn-error">{{ __('Delete') }}</button>
+                                </form>
+                            </td>
+                        </tr>
+                    @endforeach
                 </tbody>
             </table>
             {!! $productGroups->links() !!}
