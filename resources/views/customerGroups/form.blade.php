339a4e1dccab1b96677f7df96baeaf7157d2a8b4
DNAliyari <dnaliyari@jooyeshgar.com>
2024-11-04 08:51:19 +0330

    change in customer group form

--- resources/views/customerGroups/form.blade.php
+++ resources/views/customerGroups/form.blade.php
@@ -1,37 +1,18 @@
 <div class="grid grid-cols-2 gap-6">
 
     <div class="col-span-2 md:col-span-1">
-        <label id="code" class="input input-bordered flex items-center gap-2">
-            کد
-            <input type="text" id="code" name="code"
-                   class="grow" value="{{ old('code', $customerGroup->code ?? '') }}"
-                   placeholder="کد" required/>
-        </label>
+        <x-input name="code" id="code" title="{{ __('Code') }}" :value="old('code', $customerGroup->code ?? '')"
+            placeholder="{{ __('Please insert code') }}" />
     </div>
 
     <div class="col-span-2 md:col-span-1">
-        <label id="name" class="input input-bordered flex items-center gap-2">
-            نام
-            <input type="text" id="name" name="name"
-                   class="grow" value="{{ old('name', $customerGroup->name ?? '') }}"
-                   placeholder="نام" required/>
-        </label>
+        <x-input name="name" id="name" title="{{ __('Name') }}" :value="old('name', $customerGroup->name ?? '')"
+            placeholder="{{ __('Please enter name') }}" />
     </div>
 
     <div class="col-span-2">
-
-        <label id="description" class="form-control">
-            <span class="label">
-                <span class="label-text">توضیحات</span>
-            </span>
-            <textarea id="description" name="description"
-                      class="textarea textarea-bordered h-24" placeholder="توضیحات">{{ old('name', $customerGroup->description ?? '') }}</textarea>
-        </label>
-
+        <x-textarea name="description" id="description" title="{{ __('Description') }}"
+            placeholder="{{ __('Please enter the desc') }}" :value="old('description', $customerGroup->description ?? '')" />
     </div>
 
 </div>
-
-
-
-
