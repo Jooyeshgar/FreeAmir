61420a737e551b627bc4d633fed1f16e09b78294
DNAliyari <dnaliyari@jooyeshgar.com>
2024-11-10 15:51:59 +0330

    add csrf_token to meta

--- resources/views/components/app-layout.blade.php
+++ resources/views/components/app-layout.blade.php
@@ -3,6 +3,7 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="csrf_token" content="{{ csrf_token() }}" />
     <title>{{ config('app.name') }}</title>
     @vite(['resources/css/app.css', 'resources/js/app.js'])
 
