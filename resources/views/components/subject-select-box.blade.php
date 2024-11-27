11a15d588448c5dd7ecb9cbdc85c12e0d51f7677
mamadzv <zandevakili4@gmail.com>
2024-11-27 13:09:48 +0330

    Document create page, bugs fixed

--- resources/views/components/subject-select-box.blade.php
+++ resources/views/components/subject-select-box.blade.php
@@ -4,7 +4,7 @@
     <div class="selfSelectBox hidden absolute z-[3] top-[40px] w-full h-[300px] bg-white overflow-auto px-4 pb-4 rounded-[16px] shadow-[0px_43px_27px_0px_#00000012]">
         <div class="sticky top-0 left-0 right-0 w-full bg-white py-2">
             <div class="relative">
-                <x-text-input name="" value="" id="searchInput" label_text_class="text-gray-500" label_class="w-full" input_class="pe-8 text-sm"
+                <x-text-input name="" value="" label_text_class="text-gray-500" label_class="w-full" input_class="pe-8 text-sm searchInput"
                     placeholder="{{ __('Search... (heading code or name)') }}"></x-text-input>
 
                 <span class="absolute block left-2 top-1/2 translate-y-[-50%]">
@@ -28,9 +28,9 @@
                 </span>
             </div>
 
-            <div class="mt-4 text-xs hidden" id="searchResultDiv">
+            <div class="mt-4 text-xs hidden searchResultDiv">
             </div>
-            <div class="mt-4 text-xs" id="resultDiv">
+            <div class="mt-4 text-xs resultDiv">
                 @foreach ($subjects as $subject)
                     <div class="w-full ps-2 mb-4">
                         <div class="flex justify-between">
@@ -66,121 +66,3 @@
     </div>
 
 </div>
-
-<script>
-    const csrf = document.querySelector('meta[name="csrf_token"]').getAttribute('content')
-    const searchInput = document.getElementById('searchInput')
-    let resultDiv = document.getElementById("resultDiv")
-    let searchResultDiv = document.getElementById("searchResultDiv")
-
-    function debounce(func, delay) {
-        let timeout
-        return function(...args) {
-            clearTimeout(timeout)
-            timeout = setTimeout(() => {
-                func.apply(this, args)
-            }, delay);
-        }
-    }
-
-    function searchQuery(query) {
-        fetch("/subjects/search", {
-                method: "POST",
-                headers: {
-                    "Content-Type": "application/json",
-                    "X-CSRF-Token": csrf
-                },
-                body: JSON.stringify({
-                    query
-                })
-            })
-            .then(response => {
-                if (!response.ok) {
-                    throw new Error("خطا در دریافت پاسخ")
-                }
-                return response.json()
-            })
-            .then(data => {
-                resultDiv.style.display = "none"
-                searchResultDiv.style.display = "block"
-                if (data.length == 0) {
-                    searchResultDiv.innerHTML = '<span class="block text-center">چیزی پیدا نشد!</span>'
-                } else {
-                    data.forEach(element => {
-                        let targetName = element.name
-                        let targetCode = element.code
-                        let counter = 0
-                        if (element.sub_subjects.length == 0) {
-                            let createElement = `
-                        <div class="w-full ps-2 mb-4">
-                            <div class="flex justify-between">
-                                <span>
-                                    ${targetName}
-                                </span>
-
-                                <span>
-                                    ${targetCode}
-                                </span>
-                            </div>
-                        </div>
-                        `
-                            searchResultDiv.innerHTML = createElement
-                        } else {
-                            let subs = element.sub_subjects
-                            let createElement = `
-                        <div class="w-full ps-2 mb-4">
-                            <div class="flex justify-between">
-                                <span>
-                                    ${targetName}
-                                </span>
-
-                                <span>
-                                    ${targetCode}
-                                </span>
-                            </div>
-                        </div>
-                        <div class="ps-1 mt-4">
-                            <div class="border-s-[1px] ps-7 border-[#ADB5BD]" id="sub-${counter}"></div>
-                        </div>
-                        `
-                            searchResultDiv.innerHTML = createElement
-
-                            subs.forEach(sub => {
-                                console.log(sub)
-                                let subContainer = document.getElementById(`sub-${counter}`)
-                                let createElement = `
-                                    <a href="javascript:void(0)"
-                                        class="selfSelectBoxItems flex justify-between mb-4"
-                                        onclick="fillInput(this, '0')">
-                                        <span class="selfItemTitle">
-                                            ${sub.name}
-                                        </span>
-                                        <span class="selfItemCode">
-                                            ${sub.code}
-                                        </span>
-                                        <span class="selfItemId hidden">{{ $children->id }}</span>
-                                    </a>
-                                    `
-                                subContainer.innerHTML += createElement
-                            })
-                            counter += 1
-                        }
-                    });
-                }
-            })
-            .catch(error => {
-                console.error("خطایی رخ داده: ", error)
-            })
-    }
-
-    const debouncedSearch = debounce(function(event) {
-        const query = event.target.value
-        if (query) {
-            searchQuery(query)
-        } else {
-            resultDiv.style.display = "block"
-            searchResultDiv.style.display = "none"
-        }
-    }, 500)
-    searchInput.addEventListener('input', debouncedSearch)
-</script>
