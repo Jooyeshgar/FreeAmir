3b489a8eb8344f75f5e5c85db0572aa34d14425c
yasin <yas.am.1390@gmail.com>
2025-09-25 14:38:23 +0330

    Add model, seeder, factory and controller for Websites in product.

---
+++ database/migrations/2025_09_25_091853_create_product_websites_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('product_websites', function (Blueprint $table) {
+            $table->id();
+            $table->string('link');
+            $table->unsignedBigInteger('product_id');
+            $table->timestamps();
+
+            $table->foreign('product_id')->references('id')->on('products')->onDelete('set null');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('product_websites');
+    }
+};
