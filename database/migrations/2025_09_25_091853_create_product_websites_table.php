bc257270c0cead25e648b1acde109431edfa0700
yasin <yas.am.1390@gmail.com>
2025-09-25 15:35:32 +0330

    Add component for add, edit or delete websites of product

--- database/migrations/2025_09_25_091853_create_product_websites_table.php
+++ database/migrations/2025_09_25_091853_create_product_websites_table.php
@@ -17,7 +17,7 @@ return new class extends Migration
             $table->unsignedBigInteger('product_id');
             $table->timestamps();
 
-            $table->foreign('product_id')->references('id')->on('products')->onDelete('set null');
+            $table->foreign('product_id')->references('id')->on('products')->cascadeOnDelete();
         });
     }
 
