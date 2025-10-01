1f4dd5a2de77de0d9d6c2841eb9b1d9a54c17ad7
yasin <yas.am.1390@gmail.com>
2025-10-01 11:18:27 +0330

    Configs completed.

--- resources/views/configs/index.blade.php
+++ resources/views/configs/index.blade.php
@@ -7,18 +7,39 @@
     <x-show-message-bags />
     <div class="card bg-base-100 shadow-xl">
         <div class="card-body">
-            <form action="{{ route('configs.store') }}" method="POST" enctype="multipart/form-data">
-                @csrf
-                <div class="card-body">
-                    <div class="card-title">{{ __('Edit Config') }}</div>
-                    <x-show-message-bags />
+            <div class="card-body">
+                <div class="card-title">{{ __('Edit Config') }}</div>
+                <x-show-message-bags />
 
-                    @include('configs.form')
-                    <div class="card-actions">
-                        <button type="submit" class="btn btn-pr"> {{ __('Edit') }} </button>
+                <x-slot name="header">
+                    <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+                        {{ __('Configs') }}
+                    </h2>
+                </x-slot>
+                <x-show-message-bags />
+                <div class="card bg-base-100 shadow-xl">
+                    <div class="card-body">
+                        <table class="table w-full mt-4 overflow-auto">
+                            <thead>
+                                <tr>
+                                    <th class="px-4 py-2">{{ __('Subject') }}</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                @foreach ($configs as $config)
+                                    <tr>
+                                        <td class="px-4 py-2">{{ $config->desc ?? '' }}</td>
+                                        <td class="px-4 py-2">
+                                            <a href="{{ route('configs.edit', $config->id) }}"
+                                                class="btn btn-sm btn-info">{{ __('Edit') }}</a>
+                                        </td>
+                                    </tr>
+                                @endforeach
+                            </tbody>
+                        </table>
                     </div>
                 </div>
-            </form>
+
+            </div>
         </div>
-    </div>
 </x-app-layout>
