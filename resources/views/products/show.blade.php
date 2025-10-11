833bbfcfce44f5e28f063678d764187c9df9b95c
yasin <yas.am.1390@gmail.com>
2025-10-11 16:27:04 +0330

    Product show bug fixed

--- resources/views/products/show.blade.php
+++ resources/views/products/show.blade.php
@@ -160,7 +160,7 @@
 
                         @foreach ($invoice_items as $item)
                             @php
-                                if ($item->is_sell) {
+                                if ($item->invoice_type->value == 'sell') {
                                     $remaining -= $item->quantity;
                                     $totalSell += $item->quantity;
                                 } else {
@@ -177,7 +177,7 @@
                                 </td>
 
                                 <td class="px-4 py-3 text-center">
-                                    @if ($item->is_sell)
+                                    @if ($item->invoice_type->value == 'sell')
                                         <a href="{{ route('invoices.show', $item->invoice_id) }}"
                                             class="badge badge-success gap-2 hover:badge-success hover:brightness-110 transition-all">
                                             <svg xmlns="http://www.w3.org/2000/svg" class="h-4 w-4" fill="none" viewBox="0 0 24 24" stroke="currentColor">
@@ -191,7 +191,7 @@
                                 </td>
 
                                 <td class="px-4 py-3 text-center">
-                                    @if (!$item->is_sell)
+                                    @if ($item->invoice_type->value == 'buy')
                                         <a href="{{ route('invoices.show', $item->invoice_id) }}"
                                             class="badge badge-info gap-2 hover:badge-info hover:brightness-110 transition-all">
                                             <svg xmlns="http://www.w3.org/2000/svg" class="h-4 w-4" fill="none" viewBox="0 0 24 24" stroke="currentColor">
