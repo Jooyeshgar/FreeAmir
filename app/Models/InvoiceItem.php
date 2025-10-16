67da477d3573c9b6e6ca4e6b464ab6eca8790114
yasin <yas.am.1390@gmail.com>
2025-10-16 10:32:54 +0330

    Update product when an invoice item created

--- app/Models/InvoiceItem.php
+++ app/Models/InvoiceItem.php
@@ -23,14 +23,33 @@ class InvoiceItem extends Model
         'description',
     ];
 
+    public static function booted(): void
+    {
+        static::created(function ($invoice_item) {
+            $previous_quantity = $invoice_item->product->quantity;
+            $previous_average_cost = $invoice_item->product->average_cost;
+
+            $invoice_type = Invoice::select('invoice_type')->find($invoice_item->invoice_id)->invoice_type;
+
+            $total_quantity = $invoice_type->value == 'buy' ? $previous_quantity + $invoice_item->quantity : $previous_quantity - $invoice_item->quantity;
+            $total_value = $previous_quantity * $previous_average_cost + $invoice_item->quantity * $invoice_item->unit_price;
+
+            $new_average_cost = $total_value / $total_quantity;
+
+            $invoice_item->product->quantity = $total_quantity;
+            $invoice_item->product->average_cost = $invoice_type->value == 'buy' ? $new_average_cost : $previous_average_cost;
+            $invoice_item->product->update();
+        });
+    }
+
     public function invoice()
     {
-    return $this->belongsTo(Invoice::class, 'invoice_id');
+        return $this->belongsTo(Invoice::class, 'invoice_id');
     }
 
     public function product()
     {
-    return $this->belongsTo(Product::class, 'product_id');
+        return $this->belongsTo(Product::class, 'product_id');
     }
 
     public function transaction()
