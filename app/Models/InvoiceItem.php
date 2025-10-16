234e0b65af7aca2faee83345144c205b8a9c1432
yasin <yas.am.1390@gmail.com>
2025-10-16 13:12:43 +0330

    InvoiceItem edited

--- app/Models/InvoiceItem.php
+++ app/Models/InvoiceItem.php
@@ -23,24 +23,7 @@ class InvoiceItem extends Model
         'description',
     ];
 
-    public static function booted(): void
-    {
-        static::created(function ($invoice_item) {
-            $previous_quantity = $invoice_item->product->quantity;
-            $previous_average_cost = $invoice_item->product->average_cost;
-
-            $invoice_type = Invoice::select('invoice_type')->find($invoice_item->invoice_id)->invoice_type;
-
-            $total_quantity = $invoice_type->value == 'buy' ? $previous_quantity + $invoice_item->quantity : $previous_quantity - $invoice_item->quantity;
-            $total_value = $previous_quantity * $previous_average_cost + $invoice_item->quantity * $invoice_item->unit_price;
-
-            $new_average_cost = $total_value / $total_quantity;
-
-            $invoice_item->product->quantity = $total_quantity;
-            $invoice_item->product->average_cost = $invoice_type->value == 'buy' ? $new_average_cost : $previous_average_cost;
-            $invoice_item->product->update();
-        });
-    }
+    public static function booted(): void {}
 
     public function invoice()
     {
