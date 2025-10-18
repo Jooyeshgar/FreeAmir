beff4fb94417fd8742076bbb6e46604145896784
yasin <yas.am.1390@gmail.com>
2025-10-18 08:59:35 +0330

    Creating subject after creating customers and products

--- database/seeders/CustomerSeeder.php
+++ database/seeders/CustomerSeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 use App\Models\Customer;
+use App\Models\Subject;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\Event;
 
@@ -12,6 +13,17 @@ class CustomerSeeder extends Seeder
     {
         Event::fake();
 
-        Customer::factory()->count(50)->create();
+        $customers = Customer::factory()->count(50)->create();
+        foreach ($customers as $customer) {
+            Subject::create([
+                'company_id' => $customer->company_id,
+                'subjectable_type' => Customer::class,
+                'subjectable_id' => $customer->id,
+                'parent_id' => 99,
+                'code' => '012002'.$customer->id,
+                'name' => $customer->name,
+                'type' => 2,
+            ]);
+        }
     }
 }
