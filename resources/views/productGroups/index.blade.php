c860e357641d8c564d81432f7297423971992a62
yasin <yas.am.1390@gmail.com>
2025-10-29 16:46:46 +0330

    Fix column header and update product group code display in index view

--- resources/views/productGroups/index.blade.php
+++ resources/views/productGroups/index.blade.php
@@ -15,7 +15,7 @@
             <table class="table w-full mt-4 overflow-auto">
                 <thead>
                     <tr>
-                        <th class="px-4 py-2">{{ __('Account code') }}</th>
+                        <th class="px-4 py-2">{{ __('Code') }}</th>
                         <th class="px-4 py-2">{{ __('Name') }}</th>
                         <th class="px-4 py-2">{{ __('VAT') }}</th>
                         <th class="px-4 py-2">{{ __('Products') }}</th>
@@ -26,7 +26,7 @@
 
                     @foreach ($productGroups as $productGroup)
                         <tr>
-                            <td class="px-4 py-2">{{ $productGroup->subject->formattedCode() }}</td>
+                            <td class="px-4 py-2">{{ formatCode($productGroup->inventory_subject_id) }}</td>
                             <td class="px-4 py-2">{{ $productGroup->name }}</td>
                             <td class="px-4 py-2">{{ formatNumber($productGroup->vat) }}%</td>
                             <td class="px-4 py-2">{{ formatNumber($productGroup->products->count()) }}</td>
