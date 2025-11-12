bd5820e79ba938a255390a98f745c2b5d9e3420b
yasin <yas.am.1390@gmail.com>
2025-11-12 14:49:37 +0330

    Implement edit/delete invoice status check with tooltip for disabled button

--- resources/views/invoices/show.blade.php
+++ resources/views/invoices/show.blade.php
@@ -212,13 +212,29 @@
                         </svg>
                         {{ __('Print PDF') }}
                     </a>
-                    <a href="{{ route('invoices.edit', $invoice) }}" class="btn btn-primary gap-2">
-                        <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-                                d="M11 5H6a2 2 0 00-2 2v11a2 2 0 002 2h11a2 2 0 002-2v-5m-1.414-9.414a2 2 0 112.828 2.828L11.828 15H9v-2.828l8.586-8.586z" />
-                        </svg>
-                        {{ __('Edit invoice') }}
-                    </a>
+                    
+                    @php($editDeleteStatus = \App\Services\InvoiceService::getEditDeleteStatus($invoice))
+
+                    @if($editDeleteStatus['allowed'])
+                        <a href="{{ route('invoices.edit', $invoice) }}" class="btn btn-primary gap-2">
+                            <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                                    d="M11 5H6a2 2 0 00-2 2v11a2 2 0 002 2h11a2 2 0 002-2v-5m-1.414-9.414a2 2 0 112.828 2.828L11.828 15H9v-2.828l8.586-8.586z" />
+                            </svg>
+                            {{ __('Edit invoice') }}
+                        </a>
+                    @else
+                        <span class="tooltip" data-tip="{{ $editDeleteStatus['reason'] }}">
+                            <button class="btn btn-primary gap-2 btn-disabled cursor-not-allowed" disabled title="{{ $editDeleteStatus['reason'] }}">
+                                <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                                        d="M11 5H6a2 2 0 00-2 2v11a2 2 0 002 2h11a2 2 0 002-2v-5m-1.414-9.414a2 2 0 112.828 2.828L11.828 15H9v-2.828l8.586-8.586z" />
+                                </svg>
+                                {{ __('Edit invoice') }}
+                            </button>
+                        </span>
+                    @endif
+                    
                     @if ($invoice->document)
                         <a href="{{ route('documents.show', $invoice->document) }}" class="btn btn-secondary gap-2">
                             <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor">
