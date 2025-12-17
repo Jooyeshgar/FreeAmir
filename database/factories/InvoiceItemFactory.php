dfd739e839374ceab64aa5a7cb13cfdaf21ff408
Hadi <Hadi60@gmail.com>
2025-12-17 17:33:34 +0330

    add invoice factory

--- database/factories/InvoiceItemFactory.php
+++ database/factories/InvoiceItemFactory.php
@@ -2,7 +2,6 @@
 
 namespace Database\Factories;
 
-use App\Models\Invoice;
 use App\Models\Product;
 use App\Models\Transaction;
 use Illuminate\Database\Eloquent\Factories\Factory;
@@ -11,15 +10,14 @@ class InvoiceItemFactory extends Factory
 {
     public function definition(): array
     {
-        $quantity = $this->faker->randomFloat(0, 1, 10);
-        $unit_price = $this->faker->randomFloat(0, 100, 1000);
+        $quantity = $this->faker->randomFloat(1, 1, 10);
+        $unit_price = $this->faker->randomFloat(2, 100, 1000);
 
         return [
-            'invoice_id' => Invoice::inRandomOrder()->first()->id,
             'description' => $this->faker->paragraph(2),
             'item_id' => Product::inRandomOrder()->first()->id,
             'item_type' => 'product',
-            'transaction_id' => Transaction::factory()->create()->id,
+            'transaction_id' => Transaction::factory(),
             'quantity' => $quantity,
             'unit_price' => $unit_price,
             'unit_discount' => 0,
