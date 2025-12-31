449ed6d45d84fb827548541693c540e9077763e3
Hadi <Hadi60@gmail.com>
2025-12-31 18:44:00 +0330

    UI change

--- resources/views/reports/trialBalance.blade.php
+++ resources/views/reports/trialBalance.blade.php
@@ -7,7 +7,14 @@
     <x-show-message-bags />
     <div class="card bg-base-100 shadow-xl">
         <div class="card-body">
-            <div class="card-actions ">
+            <div class="card-actions flex items-center justify-between mb-4">
+                <h3 class="text-lg font-bold">
+                    @if($currentParent)
+                        {{ __('Trial Balance') }} - {{ $currentParent->name }}
+                    @else
+                        {{ __('Trial Balance at General Level') }}
+                    @endif
+                </h3>
                 @if($currentParent)
                     @php
                         $upUrl = route('reports.trial-balance');
@@ -15,61 +22,48 @@
                             $upUrl .= '?parent_id=' . $currentParent->parent_id;
                         }
                     @endphp
-
-                    <span class="ml-2 text-lg leading-[3rem] font-semibold grow">{{ $currentParent->name }}</span>
-                    <a href="{{ $upUrl }}" class="btn btn-outline">
+                    <a href="{{ $upUrl }}" class="btn btn-outline btn-sm">
                         {{ __('Back') }}
                     </a>
                 @endif
             </div>
-            <table class="table w-full mt-4">
+            <table class="table table-zebra w-full mt-4 border border-gray-300">
                 <thead>
-                    <tr>
-                        <th class="px-4 py-2">{{ __('Code') }}</th>
-                        <th class="px-4 py-2">{{ __('Name') }}</th>
-                        <th class="px-4 py-2">{{ __('Parent') }}</th>
-                        <th class="px-4 py-2">{{ __('Type') }}</th>
-                        <th class="px-4 py-2">{{ __('Debtor') }}</th>
-                        <th class="px-4 py-2">{{ __('Creditor') }}</th>
-                        <th class="px-4 py-2">{{ __('Debtor Remaining') }}</th>
-                        <th class="px-4 py-2">{{ __('Creditor Remaining') }}</th>
-                        <th class="px-4 py-2">{{ __('Action') }}</th>
+                    <tr class="bg-base-200 ">
+                        <th rowspan="2" class="px-4 py-2 text-center border-r align-middle border-gray-400">{{ __('Code') }}</th>
+                        <th rowspan="2" class="px-4 py-2 text-center border-r align-middle border-gray-400">{{ __('Name') }}</th>
+                        <th colspan="2" class="px-4 py-2 text-center border-r  border-gray-400">{{ __('Turnover') }}</th>
+                        <th colspan="2" class="px-4 py-2 text-center border-r border-gray-400">{{ __('Balance') }}</th>
+                    </tr>
+                    <tr class="bg-base-200">
+                        <th class="px-4 py-2 text-center border-r border-gray-400">{{ __('Debtor') }}</th>
+                        <th class="px-4 py-2 text-center ">{{ __('Creditor') }}</th>
+                        <th class="px-4 py-2 text-center border-r border-gray-400">{{ __('Debtor') }}</th>
+                        <th class="px-4 py-2 text-center">{{ __('Creditor') }}</th>
                     </tr>
                 </thead>
                 <tbody>
                     @foreach ($subjects as $subject)
-                        <tr>
-                            <td>
+                        <tr class="hover ">
+                            <td class="border-r border-gray-400 text-center">
                                 <a href="{{ route('transactions.index', ['subject_id' => $subject->id]) }}" class="text-primary hover:underline" title="{{ __('View transactions for this subject') }}">
                                     {{ $subject->formattedCode() }}
                                 </a>
                             </td>
-                            <td class="px-4 py-2">
+                            <td class="px-4 py-2 border-r border-gray-400">
                                 <a href="{{ route('reports.trial-balance', ['parent_id' => $subject->id]) }}" class="text-primary"> {{ $subject->name }}</a>
                                 @if ($subject->subjectable)
-                                    <div class="badge badge-primary" title="{{ __('Related to') }}: {{ class_basename($subject->subjectable::class) }}">
+                                    <div class="badge badge-primary badge-sm ml-2" title="{{ __('Related to') }}: {{ class_basename($subject->subjectable::class) }}">
                                         <a href="{{ route(model_route($subject->subjectable, 'show'), $subject->subjectable) }}">
                                         {{ __(class_basename($subject->subjectable::class)) }}
                                         </a>
                                     </div>
                                 @endif
                             </td>
-                            <td class="px-4 py-2">{{ $subject->parent ? $subject->parent->name : __('Main') }}</td>
-                            <td class="px-4 py-2">{{ $subject->type ? __(ucfirst($subject->type)) : '-' }}</td>
-                            <td class="px-4 py-2">{{ formatNumber(abs($subject->debit)) }}</td>
-                            <td class="px-4 py-2">{{ formatNumber($subject->credit) }}</td>
-                            <td class="px-4 py-2">{{ $subject->balance < 0 ? formatNumber(abs($subject->balance)) : formatNumber(0) }}</td>
-                            <td class="px-4 py-2">{{ $subject->balance > 0 ? formatNumber(abs($subject->balance)) : formatNumber(0) }}</td>
-                            <td class="px-4 py-2">
-                                <form action="{{ route('reports.result') }}" method="get">
-                                    <input type="hidden" name="report_for" value="subLedger">
-                                    <input type="hidden" name="subject_id" value="{{ $subject->id }}">
-                                    <div class="mt-2 flex gap-2">
-                                        <button type="submit" name="action" value="print" class="btn btn-default rounded-md"> {{ __('Print') }}</button>
-                                    </div>
-                                </form>
-
-                            </td>
+                            <td class="px-4 py-2 border-r border-gray-400">{{ formatNumber(abs($subject->debit)) }}</td>
+                            <td class="px-4 py-2 border-r">{{ formatNumber($subject->credit) }}</td>
+                            <td class="px-4 py-2 border-r border-gray-400">{{ $subject->balance < 0 ? formatNumber(abs($subject->balance)) : formatNumber(0) }}</td>
+                            <td class="px-4 py-2 border-r">{{ $subject->balance > 0 ? formatNumber(abs($subject->balance)) : formatNumber(0) }}</td>
                         </tr>
                     @endforeach
                 </tbody>
