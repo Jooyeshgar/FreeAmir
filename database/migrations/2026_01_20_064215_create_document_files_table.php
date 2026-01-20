401e3c1fece444d665ce02c60c85ed176794f0d9
yasin <yas.am.1390@gmail.com>
2026-01-20 11:36:48 +0330

    DocumentFile model complete except file handeling

---
+++ database/migrations/2026_01_20_064215_create_document_files_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('document_files', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('document_id')->nullable();
+            $table->unsignedBigInteger('user_id')->nullable();
+            $table->string('name', 60);
+            $table->string('title')->nullable();
+            $table->string('file_address');
+            $table->foreign('document_id')->references('id')->on('documents')->onDelete('set null');
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('set null');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('document_files');
+    }
+};
