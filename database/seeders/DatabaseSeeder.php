1952c6819c74b48cab3f80dc5e81f29693dbd074
yasin <yas.am.1390@gmail.com>
2026-01-24 15:42:26 +0330

    khosro check

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -12,6 +12,7 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
+        ;./kvdfjvio
         $this->call([
             CompanySeeder::class,
             SubjectSeeder::class,
