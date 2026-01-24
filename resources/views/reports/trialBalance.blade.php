597feafb024130f571e1087b5ecde6e55422c677
yasin <yas.am.1390@gmail.com>
2026-01-24 15:25:44 +0330

    Update route parameters and adjust form layout for better responsiveness

--- resources/views/reports/trialBalance.blade.php
+++ resources/views/reports/trialBalance.blade.php
@@ -16,7 +16,7 @@
                     @endif
                 </h3>
                 @if($currentParent)
-                    <a href="{{ route('reports.trial-balance') . '?parent_id=' . $currentParent->parent_id }}" class="btn btn-outline btn-sm">{{ __('Go Up').' - '.( !is_null($currentParent->parent) ? $currentParent->parent->name : __('General Level') ) }} </a>
+                    <a href="{{ route('reports.trial-balance', array_merge(request()->query(), ['parent_id' => $currentParent->parent_id])) }}" class="btn btn-outline btn-sm">{{ __('Go Up').' - '.( !is_null($currentParent->parent) ? $currentParent->parent->name : __('General Level') ) }} </a>
                 @endif
             </div>
             <form action="{{ route('reports.trial-balance') }}" method="get">
@@ -25,26 +25,24 @@
                 @endif
 
                 <div class="grid grid-cols-12 gap-3 items-end">
-                    <div class="col-span-6 sm:col-span-3">
-                        <x-date-picker name="start_date" value="{{ $start_date }}" class="w-full" placeholder="{{ __('Start date') }}"></x-date-picker>
+                    <div class="col-span-2">
+                        <x-date-picker name="start_date" value="{{ $start_date }}" class="w-full" placeholder="{{ __('Start date') }}" />
                     </div>
-                    <div class="col-span-6 sm:col-span-3">
-                        <x-date-picker name="end_date" value="{{ $end_date }}" class="w-full" placeholder="{{ __('End date') }}"></x-date-picker>
+                    <div class="col-span-2">
+                        <x-date-picker name="end_date" value="{{ $end_date }}" class="w-full" placeholder="{{ __('End date') }}" />
                     </div>
-                    <div class="col-span-6 sm:col-span-3">
-                        <x-input name="start_document_number" value="{{ $start_document_number ?? 3 }}" class="w-full" placeholder="{{ __('Document start number') }}"></x-input>
+                    <div class="col-span-2">
+                        <x-input name="start_document_number" value="{{ $start_document_number ?? 3 }}" class="w-full" placeholder="{{ __('Document start number') }}" />
                     </div>
-                    <div class="col-span-6 sm:col-span-3">
-                        <x-input name="end_document_number" value="{{ $end_document_number }}" class="w-full" placeholder="{{ __('Document end number') }}"></x-input>
+                    <div class="col-span-2">
+                        <x-input name="end_document_number" value="{{ $end_document_number }}" class="w-full" placeholder="{{ __('Document end number') }}" />
                     </div>
-                    <div class="col-span-12 sm:col-span-3 flex flex-col gap-2">
-                        <label class="flex items-center gap-2 text-sm text-gray-700">
-                            <input type="checkbox" name="include_children" value="1" class="checkbox checkbox-sm" {{ $include_children ? 'checked' : '' }}>
-                            <span>{{ __('Include 2 levels') }}</span>
-                        </label>
+                    <div class="col-span-2 flex items-center gap-3">
+                        <input type="checkbox" name="include_children" value="1" class="checkbox checkbox-md" {{ $include_children ? 'checked' : '' }}>
+                        <span class="text-gray-700">{{ __('Include 2 levels') }}</span>
                     </div>
-                    <div class="col-span-12 sm:col-span-3 flex gap-2 sm:justify-end">
-                        <button type="submit" class="btn text-white btn-primary rounded-md"> {{ __('Search') }}</button>
+                    <div class="col-span-2 flex gap-2 justify-end">
+                        <button type="submit" class="btn btn-primary text-white rounded-md">{{ __('Search') }}</button>
                         <a href="{{ route('reports.trial-balance') }}" class="btn btn-outline rounded-md">{{ __('Clear') }}</a>
                     </div>
                 </div>
@@ -84,7 +82,7 @@
                                         @if ($depth > 0)
                                             <span class="w-2 h-2 rounded-full bg-gray-400 inline-block"></span>
                                         @endif
-                                        <a href="{{ route('reports.trial-balance', ['parent_id' => $subject->id]) }}" class="text-primary">{{ $subject->name }}</a>
+                                        <a href="{{ route('reports.trial-balance', array_merge(request()->query(), ['parent_id' => $subject->id])) }}" class="text-primary">{{ $subject->name }}</a>
                                         @if ($subject->subjectable)
                                             <div class="badge badge-primary badge-sm ml-2" title="{{ __('Related to') }}: {{ class_basename($subject->subjectable::class) }}">
                                                 <a href="{{ route(model_route($subject->subjectable, 'show'), $subject->subjectable) }}">
