ca9e963baf590db9ccd850d44152c85befdf8ea0
yasin <yas.am.1390@gmail.com>
2026-01-25 16:04:33 +0330

    Refactor sell and warehouse chart components to use consistent dataset structure

--- resources/views/components/charts/sell-chart.blade.php
+++ resources/views/components/charts/sell-chart.blade.php
@@ -1,3 +1,3 @@
 @props(['datas', 'chartId' => null])
 
-<x-charts.bar-chart :datas="$datas" :chart-id="$chartId ?? ($attributes->get('id') ?? 'sellChart')" heightClass="h-64" />
+<x-charts.bar-chart chart-id="sellChart" :datasets="[['data' => $datas]]" />
--- resources/views/components/charts/warehouse-chart.blade.php
+++ resources/views/components/charts/warehouse-chart.blade.php
@@ -1,3 +1,3 @@
 @props(['datas', 'chartId' => null])
 
-<x-charts.bar-chart :datas="$datas" :chart-id="$chartId ?? ($attributes->get('id') ?? 'warehouseChart')" heightClass="h-96" />
+<x-charts.bar-chart chart-id="warehouseChart" heightClass="h-96" :datasets="[['data' => $datas]]" />
