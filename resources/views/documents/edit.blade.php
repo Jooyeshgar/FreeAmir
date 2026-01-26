81870c2f813153ad8d2b6c2b587d4fe87e7aaeba
yasin <yas.am.1390@gmail.com>
2026-01-26 11:48:53 +0330

    Limit subjects in DocumentController && fix search route

--- resources/views/documents/edit.blade.php
+++ resources/views/documents/edit.blade.php
@@ -1,12 +1,12 @@
 <x-app-layout>
     <x-slot name="header">
         <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            {{ __('Create Transaction') }}
+            {{ __('Edit Transaction') }}
         </h2>
     </x-slot>
     <div class="font-bold text-gray-600 py-6 text-2xl">
         <span>
-            ثبت سند حسابداری
+            {{ __('Registration of accounting document') }}
         </span>
     </div>
     <div class="">
