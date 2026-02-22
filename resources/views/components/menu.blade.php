920ff5165588f3aa02ee19f51596079c76f9741e
Hadi <Hadi60@gmail.com>
2026-02-22 17:36:26 +0330

    UI improvement

--- resources/views/components/menu.blade.php
+++ resources/views/components/menu.blade.php
@@ -69,45 +69,22 @@
 <li class="dropdown dropdown-hover">
     <div tabindex="4" role="button">{{ __('Management') }}</div>
     <ul tabindex="4" class="dropdown-content menu bg-base-100 rounded-box z-[1] w-52 p-2 shadow">
+
+        {{-- HR & Organization --}}
         @canany(['org-charts.index', 'org-charts.create', 'org-charts.edit'])
-            <li><a href="{{ route('org-charts.index') }}">{{ __('Organization Chart') }}</a></li>
+            <li>
+                <details>
+                    <summary>{{ __('HR & Organization') }}</summary>
+                    <ul>
+                        @canany(['org-charts.index', 'org-charts.create', 'org-charts.edit'])
+                            <li><a href="{{ route('org-charts.index') }}">{{ __('Organization Chart') }}</a></li>
+                        @endcanany
+                    </ul>
+                </details>
+            </li>
         @endcanany
-        @canany(['subjects.index', 'subjects.create', 'subjects.edit'])
-            <li><a href="{{ route('subjects.index') }}">{{ __('Subjects') }}</a></li>
-        @endcanany
-        @canany(['bank-accounts.index', 'bank-accounts.create', 'bank-accounts.edit'])
-            <li><a href="{{ route('bank-accounts.index') }}">{{ __('Bank Accounts') }}</a></li>
-        @endcanany
-        @canany(['customers.index', 'customers.create', 'customers.edit'])
-            <li><a href="{{ route('customers.index') }}">{{ __('Customers') }} </a></li>
-        @endcanany
-        @canany(['customer-groups.index', 'customer-groups.create', 'customer-groups.edit'])
-            <li><a href="{{ route('customer-groups.index') }}">{{ __('Customer Groups') }} </a></li>
-        @endcanany
-        @canany(['services.index', 'services.create', 'services.edit'])
-            <li><a href="{{ route('services.index') }}">{{ __('Services') }}</a></li>
-        @endcanany
-        @canany(['service-groups.index', 'service-groups.create', 'service-groups.edit'])
-            <li><a href="{{ route('service-groups.index') }}">{{ __('Service Groups') }}</a></li>
-        @endcanany
-        @canany(['companies.index', 'companies.create', 'companies.edit'])
-            <li><a href="{{ route('companies.index') }}">{{ __('Companies') }} </a></li>
-        @endcanany
-        @canany(['banks.index', 'banks.create', 'banks.edit'])
-            <li><a href="{{ route('banks.index') }}">{{ __('Banks') }}</a></li>
-        @endcanany
-        @canany(['management.users.index', 'management.users.create', 'management.users.edit'])
-            <li><a href="{{ route('users.index') }}">{{ __('Users') }}</a></li>
-        @endcanany
-        @canany(['management.permissions.index', 'management.permissions.create', 'management.permissions.edit'])
-            <li><a href="{{ route('permissions.index') }}">{{ __('Permissions') }}</a></li>
-        @endcanany
-        @canany(['management.roles.index', 'management.roles.create', 'management.roles.edit'])
-            <li><a href="{{ route('roles.index') }}">{{ __('Roles') }}</a></li>
-        @endcanany
-        @canany(['management.configs.index', 'management.configs.create', 'management.configs.edit'])
-            <li><a href="{{ route('configs.index') }}">{{ __('Configs') }}</a></li>
-        @endcanany
+
+        {{-- Salary --}}
         @canany(['salary.tax-slabs.index', 'salary.tax-slabs.create', 'salary.tax-slabs.edit', 'salary.work-sites.index', 'salary.work-sites.create',
             'salary.work-sites.edit', 'salary.public-holidays.index', 'salary.public-holidays.create', 'salary.public-holidays.edit', 'salary.payroll-elements.index',
             'salary.payroll-elements.create', 'salary.payroll-elements.edit'])
@@ -134,6 +111,80 @@
                 </details>
             </li>
         @endcanany
+
+        {{-- Customers & Services --}}
+        @canany(['customers.index', 'customers.create', 'customers.edit', 'customer-groups.index', 'customer-groups.create', 'customer-groups.edit', 'services.index',
+            'services.create', 'services.edit', 'service-groups.index', 'service-groups.create', 'service-groups.edit'])
+            <li>
+                <details>
+                    <summary>{{ __('Customers & Services') }}</summary>
+                    <ul>
+                        @canany(['customers.index', 'customers.create', 'customers.edit'])
+                            <li><a href="{{ route('customers.index') }}">{{ __('Customers') }}</a></li>
+                        @endcanany
+                        @canany(['customer-groups.index', 'customer-groups.create', 'customer-groups.edit'])
+                            <li><a href="{{ route('customer-groups.index') }}">{{ __('Customer Groups') }}</a></li>
+                        @endcanany
+                        @canany(['services.index', 'services.create', 'services.edit'])
+                            <li><a href="{{ route('services.index') }}">{{ __('Services') }}</a></li>
+                        @endcanany
+                        @canany(['service-groups.index', 'service-groups.create', 'service-groups.edit'])
+                            <li><a href="{{ route('service-groups.index') }}">{{ __('Service Groups') }}</a></li>
+                        @endcanany
+                    </ul>
+                </details>
+            </li>
+        @endcanany
+
+        {{-- Finance --}}
+        @canany(['bank-accounts.index', 'bank-accounts.create', 'bank-accounts.edit', 'banks.index', 'banks.create', 'banks.edit', 'subjects.index', 'subjects.create',
+            'subjects.edit'])
+            <li>
+                <details>
+                    <summary>{{ __('Finance') }}</summary>
+                    <ul>
+                        @canany(['subjects.index', 'subjects.create', 'subjects.edit'])
+                            <li><a href="{{ route('subjects.index') }}">{{ __('Subjects') }}</a></li>
+                        @endcanany
+                        @canany(['bank-accounts.index', 'bank-accounts.create', 'bank-accounts.edit'])
+                            <li><a href="{{ route('bank-accounts.index') }}">{{ __('Bank Accounts') }}</a></li>
+                        @endcanany
+                        @canany(['banks.index', 'banks.create', 'banks.edit'])
+                            <li><a href="{{ route('banks.index') }}">{{ __('Banks') }}</a></li>
+                        @endcanany
+                    </ul>
+                </details>
+            </li>
+        @endcanany
+
+        {{-- System --}}
+        @canany(['companies.index', 'companies.create', 'companies.edit', 'management.users.index', 'management.users.create', 'management.users.edit',
+            'management.permissions.index', 'management.permissions.create', 'management.permissions.edit', 'management.roles.index', 'management.roles.create',
+            'management.roles.edit', 'management.configs.index', 'management.configs.create', 'management.configs.edit'])
+            <li>
+                <details>
+                    <summary>{{ __('System') }}</summary>
+                    <ul>
+                        @canany(['companies.index', 'companies.create', 'companies.edit'])
+                            <li><a href="{{ route('companies.index') }}">{{ __('Companies') }}</a></li>
+                        @endcanany
+                        @canany(['management.users.index', 'management.users.create', 'management.users.edit'])
+                            <li><a href="{{ route('users.index') }}">{{ __('Users') }}</a></li>
+                        @endcanany
+                        @canany(['management.permissions.index', 'management.permissions.create', 'management.permissions.edit'])
+                            <li><a href="{{ route('permissions.index') }}">{{ __('Permissions') }}</a></li>
+                        @endcanany
+                        @canany(['management.roles.index', 'management.roles.create', 'management.roles.edit'])
+                            <li><a href="{{ route('roles.index') }}">{{ __('Roles') }}</a></li>
+                        @endcanany
+                        @canany(['management.configs.index', 'management.configs.create', 'management.configs.edit'])
+                            <li><a href="{{ route('configs.index') }}">{{ __('Configs') }}</a></li>
+                        @endcanany
+                    </ul>
+                </details>
+            </li>
+        @endcanany
+
         <li><a href="https://github.com/Jooyeshgar/FreeAmir/issues">{{ __('Support') }}</a></li>
     </ul>
 </li>
