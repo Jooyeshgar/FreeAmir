13abb9484cf7d458bf144809ca8f5741229e542e
yasin <yas.am.1390@gmail.com>
2026-03-15 09:33:48 +0330

    add new models as new sections to  FiscalYearSection

--- app/Enums/FiscalYearSection.php
+++ app/Enums/FiscalYearSection.php
@@ -6,35 +6,42 @@ use Illuminate\Support\Facades\Lang;
 
 enum FiscalYearSection: string
 {
-	case CONFIGS = 'configs';
-	case BANKS = 'banks';
-	case BANK_ACCOUNTS = 'bank_accounts';
-	case CUSTOMERS = 'customers'; // Represents both customer_groups + customers
-	case PRODUCTS = 'products'; // Represents both product_groups + products
-	case SUBJECTS = 'subjects';
-	case DOCUMENTS = 'documents'; // Represents both documents + transactions
+    case CONFIGS = 'configs';
+    case BANKS = 'banks';
+    case BANK_ACCOUNTS = 'bank_accounts';
+    case CUSTOMERS = 'customers'; // Represents customer_groups + customers + comments
+    case PRODUCTS = 'products'; // Represents product_groups + products
+    case SERVICES = 'services'; // Represents service_groups + services
+    case SUBJECTS = 'subjects';
+    case DOCUMENTS = 'documents'; // Represents documents + transactions + document_files
+    case INVOICES = 'invoices'; // Represents invoices + ancillary_costs
+    case CHEQUES = 'cheques'; //  Represents cheques + cheques_history
+    case EMPLOYEE = 'employee'; // Represents employees + org_charts + work_sites + work_site_contracts + work_shifts + salary_decrees + monthly_attendances + attendance_logs + payrolls + personnel_requests
 
-	public function label(): string
-	{
-		return Lang::get($this->value);
-	}
+    public function label(): string
+    {
+        return Lang::get($this->value);
+    }
 
-	public static function cli(): array
-	{
-		return array_reduce(self::cases(), function ($carry, $case) {
-			$carry[$case->value] = $case->label();
-			return $carry;
-		}, []);
-	}
+    public static function cli(): array
+    {
+        return array_reduce(self::cases(), function ($carry, $case) {
+            $carry[$case->value] = $case->label();
 
-	public static function ui(): array
-	{
-		return [
-			self::CONFIGS->value => self::CONFIGS->label(),
-			self::BANKS->value => self::BANKS->label(),
-			self::CUSTOMERS->value => self::CUSTOMERS->label(),
-			self::PRODUCTS->value => self::PRODUCTS->label(),
-			self::SUBJECTS->value => self::SUBJECTS->label(),
-		];
-	}
+            return $carry;
+        }, []);
+    }
+
+    public static function ui(): array
+    {
+        return [
+            self::CONFIGS->value => self::CONFIGS->label(),
+            self::BANKS->value => self::BANKS->label(),
+            self::CUSTOMERS->value => self::CUSTOMERS->label(),
+            self::PRODUCTS->value => self::PRODUCTS->label(),
+            self::SERVICES->value => self::SERVICES->label(),
+            self::SUBJECTS->value => self::SUBJECTS->label(),
+            self::EMPLOYEE->value => self::EMPLOYEE->label(),
+        ];
+    }
 }
