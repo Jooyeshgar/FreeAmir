c803c39e3622d4132ccb8939ad50e3c20227c792
yasin <yas.am.1390@gmail.com>
2026-03-31 13:53:04 +0330

    Speed increased: from 100s to 10s && ReturnInvoiceValidationTest will fix in "Return Buy & Sell problem" issue

--- tests/Feature/ReturnInvoiceValidationTest.php
+++ tests/Feature/ReturnInvoiceValidationTest.php
@@ -11,8 +11,11 @@ use App\Models\User;
 use App\Services\AncillaryCostService;
 use App\Services\CostOfGoodsService;
 use Cookie;
-use Database\Seeders\DatabaseSeeder;
-use Database\Seeders\DemoSeeder;
+use Database\Seeders\CompanySeeder;
+use Database\Seeders\ConfigSeeder;
+use Database\Seeders\CustomerGroupSeeder;
+use Database\Seeders\ProductGroupSeeder;
+use Database\Seeders\SubjectSeeder;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Support\Facades\Cache;
 use Tests\Helpers\InvoiceTestHelper;
@@ -35,8 +38,11 @@ class ReturnInvoiceValidationTest extends TestCase
     {
         parent::setUp();
 
-        $this->seed(DatabaseSeeder::class);
-        $this->seed(DemoSeeder::class);
+        $this->seed(CompanySeeder::class);
+        $this->seed(SubjectSeeder::class);
+        $this->seed(ConfigSeeder::class);
+        $this->seed(CustomerGroupSeeder::class);
+        $this->seed(ProductGroupSeeder::class);
 
         $this->companyId = Company::query()->orderBy('id')->value('id') ?? 1;
 
