14a745fd183fb0848c30cb66ea39de6ef5065f47
Hadi <Hadi60@gmail.com>
2026-05-05 10:43:26 +0330

    minor fix

--- resources/views/components/login-layout.blade.php
+++ resources/views/components/login-layout.blade.php
@@ -1,13 +1,16 @@
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}" data-force-light="true">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <title>{{ config('app.name') }}</title>
-        @vite(['resources/css/app.css', 'resources/js/app.js'])
-    </head>
-    <body>
-        <main class="bg-gray-200 mx-auto  min-h-screen p-0 flex flex-col  ">
-            {{ $slot }}
-        </main>
-    </body>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}" data-theme="light" data-force-light="true">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>{{ config('app.name') }}</title>
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
+</head>
+
+<body>
+    <main class="bg-gray-200 mx-auto  min-h-screen p-0 flex flex-col  ">
+        {{ $slot }}
+    </main>
+</body>
+
 </html>
