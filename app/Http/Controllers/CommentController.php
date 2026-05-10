d10b25cf9eed676f332a424e37eeb191398c1e95
yasin <yas.am.1390@gmail.com>
2026-05-10 17:05:34 +0330

    Add CommentService dependency injection in CommentController

--- app/Http/Controllers/CommentController.php
+++ app/Http/Controllers/CommentController.php
@@ -5,9 +5,12 @@ namespace App\Http\Controllers;
 use App\Http\Requests\StoreCommentRequest;
 use App\Models\Comment;
 use App\Models\Customer;
+use App\Services\CommentService;
 
 class CommentController extends Controller
 {
+    public function __construct(private readonly CommentService $service) {}
+
     public function index(Customer $customer)
     {
         $comments = Comment::where('customer_id', $customer->id)->paginate(25);
