c3e0a4edef1afd5bea6e7c2866a1728a2d6afaca
Hesam <hesmirahmadi@gmail.com>
2026-06-02 14:55:26 -0230

    Fix exe's database

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -8,6 +8,8 @@ use Illuminate\Pagination\Paginator;
 use Illuminate\Support\Facades\App;
 use Illuminate\Support\Facades\Gate;
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\Artisan;
+use Illuminate\Support\Facades\File;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -35,17 +37,22 @@ class AppServiceProvider extends ServiceProvider
     /**
      * Bootstrap any application services.
      */
-    public function boot(): void
+   public function boot(): void
     {
-        Paginator::defaultView('vendor.pagination.daisyui');
-        Paginator::defaultSimpleView('vendor.pagination.daisyui-simple');
-
-        App::setLocale(config('app.locale', 'fa'));
-
-        Gate::before(function ($user, $ability) {
-            if ($user->hasRole('Super-Admin')) {
-                return true;
+        $dbPath = storage_path('nativephp.sqlite');
+        
+        if (!File::exists($dbPath)) {
+            File::ensureDirectoryExists(dirname($dbPath));
+            
+            try {
+                File::put($dbPath, '');
+                Artisan::call('migrate', ['--force' => true]);
+                Artisan::call('db:seed', ['--force' => true]);
+            } catch (\Exception $e) {
+                File::delete($dbPath);
+                throw $e;
             }
-        });
-    }
+        }
+
+    }   
 }
