48d480b0b9a724efb3e5a942e85b13d07a9dd594
yasin <yas.am.1390@gmail.com>
2026-07-22 14:33:20 +0330

    Refactor AppServiceProvider for improved readability and role-based access control

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,6 +4,7 @@ namespace App\Providers;
 
 use App\Faker\PersianProductProvider;
 use App\Faker\PersianServiceProvider;
+use Faker\Generator;
 use Illuminate\Pagination\Paginator;
 use Illuminate\Support\Facades\App;
 use Illuminate\Support\Facades\Gate;
@@ -16,7 +17,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register(): void
     {
-        $this->app->afterResolving(\Faker\Generator::class, function (\Faker\Generator $faker) {
+        $this->app->afterResolving(Generator::class, function (Generator $faker) {
             $registered = [];
             foreach ($faker->getProviders() as $provider) {
                 $registered[get_class($provider)] = true;
@@ -35,7 +36,7 @@ class AppServiceProvider extends ServiceProvider
     /**
      * Bootstrap any application services.
      */
-   public function boot(): void
+    public function boot(): void
     {
         Paginator::defaultView('vendor.pagination.daisyui');
         Paginator::defaultSimpleView('vendor.pagination.daisyui-simple');
@@ -43,10 +44,10 @@ class AppServiceProvider extends ServiceProvider
         App::setLocale(config('app.locale', 'fa'));
 
         Gate::before(function ($user, $ability) {
-            if ($ability !== 'access-super-admin-panel' && $user->can('access-super-admin-panel')) {
+            if ($user->hasRole('Super-Admin')) {
                 return true;
             }
         });
 
-    }   
+    }
 }
