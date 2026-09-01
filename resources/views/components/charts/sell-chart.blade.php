7ee263e3de1ad26d2621f029d421e259ab94b94f
yasin <yas.am.1390@gmail.com>
2026-09-01 11:54:51 +0330

    Refactor chart components for improved responsiveness and consistency in styling

--- resources/views/components/charts/sell-chart.blade.php
+++ resources/views/components/charts/sell-chart.blade.php
@@ -1,3 +1,3 @@
 @props(['datas', 'chartId' => null])
 
-<x-charts.bar-chart chart-id="sellChart" :datasets="[['data' => $datas]]" height-class="h-72" />
+<x-charts.bar-chart chart-id="sellChart" :datasets="[['data' => $datas]]" height-class="h-56 sm:h-64 lg:h-72" />
